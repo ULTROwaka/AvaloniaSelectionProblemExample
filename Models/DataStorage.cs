b2fd8b1721cb68fe55b6a010df6f1f282a0587ec
Никита <click.95@mail.ru>
2021-12-23 15:55:39 +0700

    Добавьте файлы проекта.

---
+++ Models/DataStorage.cs
@@ -0,0 +1,29 @@
+﻿using DynamicData;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaSelectionProblemExample.Models
+{
+    public class DataStorage
+    {
+        private readonly SourceList<string> _items;
+        public IObservable<IChangeSet<string>> Characters => _items.Connect();
+        public DataStorage()
+        {
+            var data = new List<string>()
+            {
+                "A",
+                "BB",
+                "CCC",
+                "DDDD"
+            };
+
+            _items = new SourceList<string>();
+            _items.AddRange(data);
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+using AvaloniaSelectionProblemExample.Models;
+
+using DynamicData;
+using DynamicData.Binding;
+
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace AvaloniaSelectionProblemExample.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly DataStorage _dataStorage;
+        public Array Directions { get; set; }
+        [Reactive]
+        public SortDirection SortingDirection { get; set; }
+        private readonly ReadOnlyObservableCollection<string> _itemsList;
+        public ReadOnlyObservableCollection<string> ItemsList => _itemsList;
+        [Reactive]
+        public string? SelectedItem { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Directions = Enum.GetValues(typeof(SortDirection));
+
+            _dataStorage = new DataStorage();
+
+            var sort = this.WhenAnyValue(x => x.SortingDirection)
+               .Select(BuildThemeSort!);
+
+            _dataStorage.Characters
+                .RefCount()
+                .Sort(sort)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Bind(out _itemsList)
+                .Subscribe();
+        }
+
+        private SortExpressionComparer<string> BuildThemeSort(SortDirection direction)
+        {
+            switch (direction)
+            {
+                case SortDirection.Ascending:
+                    return SortExpressionComparer<string>.Ascending(v => v.Length);
+                case SortDirection.Descending:
+                    return SortExpressionComparer<string>.Descending(v => v.Length);
+                default:
+                    return SortExpressionComparer<string>.Descending(v => v.Length);
+            }
+            
+        }
+    }
+
+    public enum SortDirection
+    {
+        Ascending,
+        Descending
+    }
+}
